de447d8879ce9c75985b020afe3853feee1ddedb
David Sulc <davidsulc@gmail.com>
2019-06-22 14:11:47 +0200

    removed example code

--- lib/polite_client.ex
+++ lib/polite_client.ex
@@ -2,19 +2,5 @@ defmodule PoliteClient do
   @moduledoc """
   Documentation for PoliteClient.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PoliteClient.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   defdelegate start(key, opts \\ []), to: PoliteClient.ClientsMgr
 end
--- test/polite_client_test.exs
+++ test/polite_client_test.exs
@@ -1,8 +1,4 @@
 defmodule PoliteClientTest do
   use ExUnit.Case
   doctest PoliteClient
-
-  test "greets the world" do
-    assert PoliteClient.hello() == :world
-  end
 end
