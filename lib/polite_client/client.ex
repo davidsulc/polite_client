45ca580daed117f6c064703a6acc3a63db9e9df4
David Sulc <davidsulc@gmail.com>
2019-06-23 15:05:08 +0200

    make requests in async manner

--- lib/polite_client/client.ex
+++ lib/polite_client/client.ex
@@ -3,6 +3,8 @@ defmodule PoliteClient.Client do
 
   use GenServer
 
+  require Logger
+
   alias PoliteClient.Request
 
   @type http_client() :: (Request.t() -> {:ok, term()} | {:error, term()})
@@ -18,12 +20,33 @@ defmodule PoliteClient.Client do
 
   @impl GenServer
   def init(args) do
-    {:ok, %{http_client: Keyword.fetch!(args, :http_client)}}
+    state = %{
+      http_client: Keyword.fetch!(args, :http_client),
+      requests_in_flight: %{}
+    }
+
+    {:ok, state}
   end
 
   @impl GenServer
-  def handle_call({:request, request}, _from, %{http_client: c} = state) do
-    {:reply, c.(request), state}
+  def handle_call({:request, request}, from, %{http_client: c} = state) do
+    %Task{ref: ref} = Task.async(fn -> c.(request) end)
+    in_flight = Map.put(state.requests_in_flight, ref, from)
+
+    {:noreply, %{state | requests_in_flight: in_flight}}
+  end
+
+  @impl GenServer
+  def handle_info({ref, result}, state) when is_reference(ref) do
+    requester = Map.get(state.requests_in_flight, ref)
+    GenServer.reply(requester, result)
+    {:noreply, %{state | requests_in_flight: Map.delete(state.requests_in_flight, ref)}}
+  end
+
+  @impl GenServer
+  def handle_info(msg, state) do
+    Logger.error("Received unexpected message: #{inspect(msg)}")
+    {:noreply, state}
   end
 
   # TODO allow specifying http_client as a module/behaviour
