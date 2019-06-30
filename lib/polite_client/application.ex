cb5429f5787294cda52097f076d534fa2b6f7001
David Sulc <davidsulc@gmail.com>
2019-06-30 18:22:58 +0200

    enable giving names via app args

--- lib/polite_client/application.ex
+++ lib/polite_client/application.ex
@@ -9,15 +9,21 @@ defmodule PoliteClient.Application do
   @registry_name Registry.PoliteClient
   @task_supervisor_name PoliteClient.RequestTaskSupervisor
 
-  def start(_type, _args) do
+  def start(_type, args) do
+    partition_supervisor_name =
+      Keyword.get(args, :partition_supervisor_name, @partition_supervisor_name)
+
+    registry_name = Keyword.get(args, :registry_name, @registry_name)
+    task_supervisor_name = Keyword.get(args, :task_supervisor_name, @task_supervisor_name)
+
     children = [
-      {Registry, keys: :unique, name: @registry_name},
+      {Registry, keys: :unique, name: registry_name},
       # TODO document: call pass in `:max_partitions` value to limit concurrency
       # (wraps DynamicSupervisor's :max_children value)
       {PoliteClient.AppSup,
-       partition_supervisor_name: @partition_supervisor_name,
-       registry: @registry_name,
-       task_supervisor: @task_supervisor_name}
+       partition_supervisor_name: partition_supervisor_name,
+       registry: registry_name,
+       task_supervisor: task_supervisor_name}
     ]
 
     opts = [strategy: :rest_for_one, name: PoliteClient.Supervisor]
