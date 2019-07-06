545cd00131e502e22383874fcb999f27961f05f6
David Sulc <davidsulc@gmail.com>
2019-07-06 17:42:35 +0200

    documentation

--- lib/polite_client/health_checker.ex
+++ lib/polite_client/health_checker.ex
@@ -1,4 +1,8 @@
 defmodule PoliteClient.HealthChecker do
+  @moduledoc """
+  Functionality related to health checks.
+  """
+
   alias PoliteClient.ResponseMeta
 
   @type state :: %{
@@ -11,13 +15,41 @@ defmodule PoliteClient.HealthChecker do
   @type status :: :ok | {:suspend, suspension_duration()}
   @type internal_state :: term()
 
+  @typedoc """
+  The checker function determining whether the remote host is healthy.
+
+  Hosts in poor health shouldn't be queried for some time, to give them a chance to recover.
+
+  This function is called for every response received from a successful request performed.
+  To aid in determining whether the host is healthy or not, it may make use of an internal state which is
+  maintained exclusively for the checker function.
+
+  Every time it is called, the function will receive the current `t:PoliteClient.HealthChecker.internal_state/0`
+  and the `t:PoliteClient.ResponseMeta.t/0` corresponding to the request result.
+
+  The function should return a tuple containing the new `t:PoliteClient.HealthChecker.status/0` for the host
+  and the updated `t:PoliteClient.HealthChecker.internal_state/0`. The `status` must be one of:
+
+  - `:ok` if the host is healthy
+  - `{:suspend, duration}` if the host shouldn't be contacted for `duration` milliseconds. After the given duration,
+       the host may be contacted again (which is handled by the library)
+  - `{:suspend, :infinity}` if the host shouldn't be contacted until the partition is resumed manually via
+      `PoliteClient.resume/1`
+
+  The new `t:PoliteClient.HealthChecker.internal_state/0` value returned in the tuple will be transparently updated
+  and provided to the function on the next call.
+
+  Note that while failing request results should likely be taked in to account when making host health determinations,
+  you may want to also consider successful requests such as HTTP 5xx status codes, or HTTP 429 "Too Many Requests".
+  and degrade the host health evaluation accordingly. For example, you may decide that if more than half of the recent
+  requests resulted in a 504 "Gateway Timeout" error, you should suspend for 15 mins, but upon receiving a single
+  429 "Too Many Requests" reply you immediately suspend for 5 minutes.
+  """
   @type checker() ::
           (internal_state :: internal_state(), response_meta :: ResponseMeta.t() ->
-             {:ok | {:suspend, suspension_duration()}, new_internal_state :: internal_state()})
+             {status(), new_internal_state :: internal_state()})
 
   @type suspension_duration() :: non_neg_integer() | :infinity
-
-  # TODO use struct?
 
   @spec to_config(config :: map()) :: {:ok, state()}
   def to_config(%{} = config), do: {:ok, config}
