d2ecb8914e7f1d61d7a57317bc4043abb50ed0f6
David Sulc <davidsulc@gmail.com>
2019-07-07 17:13:26 +0200

    added docs to AllocatedRequest

--- lib/polite_client/allocated_request.ex
+++ lib/polite_client/allocated_request.ex
@@ -1,4 +1,14 @@
 defmodule PoliteClient.AllocatedRequest do
+  @moduledoc """
+  Struct representing requests that are acknowledged by the `PoliteClient`, as well
+  as related functions.
+  """
+
+  @typedoc """
+  The AllocatedRequest type.
+
+  See `%PoliteClient.AllocatedRequest{}` for information about each field of the structure.
+  """
   @type t :: %__MODULE__{
           ref: reference(),
           owner: pid(),
@@ -6,7 +16,7 @@ defmodule PoliteClient.AllocatedRequest do
         }
 
   @doc """
-  The Allocated struct.
+  The AllocatedRequest struct.
 
   It represents an HTTP request that has been acknowledged by a partition
   and will be processed asynchronously.
@@ -19,6 +29,7 @@ defmodule PoliteClient.AllocatedRequest do
   @enforce_keys [:ref, :owner, :partition]
   defstruct [:ref, :owner, :partition]
 
+  @doc "Returns true if both `%PoliteClient.AllocatedRequest{}`s have the same `ref` value."
   @spec same?(left :: t(), right :: t()) :: boolean()
   def same?(%__MODULE__{ref: ref}, %__MODULE__{ref: ref}), do: true
   def same?(_left, _right), do: false
